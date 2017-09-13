afdaa83a03fb9e058895ebebd399468d8234e3d5
alia9440@uni.sydney.edu.au <alia9440@uni.sydney.edu.au>
2017-09-13 16:13:25 +1000

    Revert "Added Edit User Page"
    This reverts commit db357f9fe5d27346977bf0c6fb810f3163ff023d.

--- run.py
+++ run.py
@@ -1,168 +1,134 @@
-from bottle import route, get, run, post, request, redirect, static_file
+from bottle import route, get, run, post, request, redirect, static_file, response
 from Crypto.Hash import MD5
 import re
-import numpy as np
 import model
+import security
 
 #-----------------------------------------------------------------------------
 # This class loads html files from the "template" directory and formats them using Python.
-# If you are unsure how this is working, just
+# If you are unsure how this is working, just 
 class FrameEngine:
-    def __init__(this,
-        template_path="templates/",
-        template_extension=".html",
-        **kwargs):
-        this.template_path = template_path
-        this.template_extension = template_extension
-        this.global_renders = kwargs
+	def __init__(this, 
+		template_path="templates/", 
+		template_extension=".html", 
+		**kwargs):
+		this.template_path = template_path
+		this.template_extension = template_extension
+		this.global_renders = kwargs
 
-    def load_template(this, filename):
-        path = this.template_path + filename + this.template_extension
-        file = open(path, 'r')
-        text = ""
-        for line in file:
-            text+= line
-        file.close()
-        return text
+	def load_template(this, filename):
+		path = this.template_path + filename + this.template_extension
+		file = open(path, 'r')
+		text = ""
+		for line in file:
+			text+= line
+		file.close()
+		return text
 
-    def simple_render(this, template, **kwargs):
-        template = template.format(**kwargs)
-        return  template
+	def simple_render(this, template, **kwargs):
+		template = template.format(**kwargs)
+		return  template
 
-    def render(this, template, **kwargs):
-        keys = this.global_renders.copy() #Not the best way to do this, but backwards compatible from PEP448, in Python 3.5+ use keys = {**this.global_renters, **kwargs}
-        keys.update(kwargs)
-        template = this.simple_render(template, **keys)
-        return template
+	def render(this, template, **kwargs):
+		keys = this.global_renders.copy() #Not the best way to do this, but backwards compatible from PEP448, in Python 3.5+ use keys = {**this.global_renters, **kwargs}
+		keys.update(kwargs)
+		template = this.simple_render(template, **keys)
+		return template
 
-    def load_and_render(this, filename, header="header", tailer="tailer", **kwargs):
-        template = this.load_template(filename)
-        rendered_template = this.render(template, **kwargs)
-        rendered_template = this.load_template(header) + rendered_template
-        rendered_template = rendered_template + this.load_template(tailer)
-        return rendered_template
+	def load_and_render(this, filename, header="header", tailer="tailer", **kwargs):
+		template = this.load_template(filename)
+		rendered_template = this.render(template, **kwargs)
+		rendered_template = this.load_template(header) + rendered_template
+		rendered_template = rendered_template + this.load_template(tailer)
+		return rendered_template
 
 #-----------------------------------------------------------------------------
 
 # Allow image loading
 @route('/img/<picture>')
 def serve_pictures(picture):
-    return static_file(picture, root='img/')
+	return static_file(picture, root='img/')
 
 # Allow CSS
 @route('/css/<css>')
 def serve_css(css):
-    return static_file(css, root='css/')
+	return static_file(css, root='css/')
 
 # Allow javascript
 @route('/js/<js>')
 def serve_js(js):
-    return static_file(js, root='js/')
-
-#-----------------------------------------------------------------------------
-
-# Check the login credentials
-def check_login(username, password):
-    login = False
-    if username != "admin": # Wrong Username
-        err_str = "Incorrect Username"
-        return err_str, login
-
-    if password != "password":
-        err_str = "Incorrect Password"
-        return err_str, login
-
-    login_string = "Logged in!"
-    login = True
-    return login_string, login
+	return static_file(js, root='js/')
 
 #-----------------------------------------------------------------------------
 # GET REQUESTS
-# Redirect to login
-@route('/')
-@route('/home')
-def index():
-    return fEngine.load_and_render("index")
-
 # Display the login page
 @get('/login')
 def login():
-    return fEngine.load_and_render("login")
+	return fEngine.load_and_render("login")
 
 # Display the registration page
 @get('/register')
 def register():
-    return fEngine.load_and_render("register")
+	return fEngine.load_and_render("register")
 
-# Display the edit user info page
-@get('/edituser')
-def edituser():
-    return fEngine.load_and_render("edituser")
+@route('/')
+@route('/home')
+def index():
+	security.is_logged_on()
+	return fEngine.load_and_render("index")
 
 @get('/sql_test')
 def sql_test():
-    return fEngine.load_and_render("sql_test", debug_text=model.get_users())
+	security.is_logged_on()
+	return fEngine.load_and_render("sql_test", debug_text=model.get_users())
 
 @get('/about')
 def about():
-    garble = ["leverage agile frameworks to provide a robust synopsis for high level overviews.",
-    "provide user generated content in real-time will have multiple touchpoints for offshoring."]
-    return fEngine.load_and_render("about", garble=np.random.choice(garble))
+	security.is_logged_on()
+	return fEngine.load_and_render("about", garble="blah")
+
 #-----------------------------------------------------------------------------
 # POST REQUESTS
 # Deal with the registration
 @post('/register')
 def do_register():
-    username = request.forms.get('username')
-    password = request.forms.get('password')
-    password2 = request.forms.get('password2')
-    role = request.forms.get('role')
+	username = request.forms.get('username')
+	password = request.forms.get('password')
+	password2 = request.forms.get('password2')
+	role = request.forms.get('role')
 
-    print(model.create_user(username,password, password2 ,role))
-    return fEngine.load_and_render("valid", flag="New user created")
+	success, reason = security.handle_register(username,password,password2,role)
+
+	if success: 
+		return fEngine.load_and_render("valid")
+	else:
+		return fEngine.load_and_render("invalid",reason=reason)
 
 # Attempt the login
 @post('/login')
 def do_login():
-    username = request.forms.get('username')
-    password = request.forms.get('password')
-    err_str, login = check_login(username, password)
-    if login:
-        return fEngine.load_and_render("valid", flag=err_str)
-    else:
-        return fEngine.load_and_render("invalid", reason=err_str)
+	#create a session for the user
+	session_id = request.get_cookie('session')
+	if session_id is None:
+		session_id=security.random_salt(30)
+		response.set_cookie('session',session_id,path='/')
+	request_ip=request.environ.get('REMOTE_ADDR')
 
-# Update current user's info
-@post('/edituser')
-def do_edituser():
-    username = request.forms.get('username')
-    # Check current password is same
-    if username:
-        model.sql('''UPDATE USER
-        SET username = ?
-        WHERE id = ?
-        ''', (username, id)
-        )
-        model.commit()
-    #if password and password == password2:
-            # Update old password to new password
+	username = request.forms.get('username')
+	password = request.forms.get('password')
 
-    # Updates role regardless of whether it's been changed
-    model.sql('''UPDATE USER
-    SET role = ?
-    WHERE id = ?
-    ''', (role, id)
-    )
-    model.commit()
+	success, reason=security.handle_login(username,password,session_id,request_ip)
 
-    return fEngine.load_and_render("valid", flag="changes committed!")
+	if success: 
+		return fEngine.load_and_render("valid")
+	else:
+		return fEngine.load_and_render("invalid",reason=reason)
 #-----------------------------------------------------------------------------
 
 fEngine = FrameEngine()
 model.create_tables()
-print('tables created')
 
 try:
-    run(host='localhost', port=8080, debug=True)
+	run(reloader=True, host='localhost', port=8080, debug=True)
 finally:
-    model.close()
+	model.close()
